2442f18f76bafb2e75c5e4dcd5f4e97436ef54d7
Alevv <134009912+Malejav18@users.noreply.github.com>
2024-10-28 22:20:26 -0500

    Update punto2.cc

--- punto2.cc
+++ punto2.cc
@@ -1,108 +1,123 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
 #include "ns3/csma-module.h"
 #include "ns3/applications-module.h"
-#include "ns3/bridge-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/flow-monitor-module.h"
 
 using namespace ns3;
 
-void ConfigurarAplicaciones(NodeContainer& clientes, NodeContainer& servidores, Ipv4InterfaceContainer& interfaces, uint16_t puerto) {
-    for (uint32_t i = 0; i < clientes.GetN(); ++i) {
-        // Configurar servidor en cada nodo del contenedor "servidores"
-        PacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), puerto + i));
-        ApplicationContainer servidorApp = sink.Install(servidores.Get(i));
-        servidorApp.Start(Seconds(0.5));
-        servidorApp.Stop(Seconds(10.0));
+int main(int argc, char *argv[]) {
+    // Crear nodos: 1 switch y 4 nodos para cada sistema, y 1 switch central
+    NodeContainer system1Switch, system2Switch, centralSwitch;
+    system1Switch.Create(1);  // Switch del sistema 1
+    system2Switch.Create(1);  // Switch del sistema 2
+    centralSwitch.Create(1);  // Switch central
 
-        // Configurar cliente en cada nodo del contenedor "clientes"
-        BulkSendHelper cliente("ns3::TcpSocketFactory", InetSocketAddress(interfaces.GetAddress(i), puerto + i));
-        cliente.SetAttribute("MaxBytes", UintegerValue(1024 * 1024));  // 1MB de datos
-        ApplicationContainer clienteApp = cliente.Install(clientes.Get(i));
-        clienteApp.Start(Seconds(1.0));
-        clienteApp.Stop(Seconds(9.5));
-    }
-}
+    NodeContainer system1Nodes, system2Nodes;
+    system1Nodes.Create(4);  // 1 Servidor y 3 Clientes del sistema 1
+    system2Nodes.Create(4);  // 1 Servidor y 3 Clientes del sistema 2
 
-int main(int argc, char* argv[]) {
-    Time::SetResolution(Time::NS);  // Establecer la resolución del tiempo en nanosegundos
-
-    // Crear los nodos finales (6 dispositivos)
-    NodeContainer nodos;
-    nodos.Create(6);
-
-    // Crear 3 switches
-    NodeContainer switches;
-    switches.Create(3);
-
-    // Crear canal CSMA para conectar nodos y switches
-    CsmaHelper csma;
-    csma.SetChannelAttribute("DataRate", DataRateValue(DataRate("1Gbps")));  // Velocidad de 1 Gbps
-    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(5000)));  // Retardo de 5000 ns
-
-    // Conectar los nodos a sus respectivos switches (2 nodos por switch)
-    NetDeviceContainer dispositivosSwitch1, dispositivosSwitch2, dispositivosSwitch3;
-
-    for (uint32_t i = 0; i < 2; ++i) {
-        // Nodos 0 y 1 -> Switch 1
-        NodeContainer grupo1(nodos.Get(i), switches.Get(0));
-        dispositivosSwitch1.Add(csma.Install(grupo1));
-
-        // Nodos 2 y 3 -> Switch 2
-        NodeContainer grupo2(nodos.Get(i + 2), switches.Get(1));
-        dispositivosSwitch2.Add(csma.Install(grupo2));
-
-        // Nodos 4 y 5 -> Switch 3
-        NodeContainer grupo3(nodos.Get(i + 4), switches.Get(2));
-        dispositivosSwitch3.Add(csma.Install(grupo3));
-    }
-
-    // Interconectar los switches entre sí
-    NetDeviceContainer interSwitch;
-    interSwitch.Add(csma.Install(NodeContainer(switches.Get(0), switches.Get(1))));
-    interSwitch.Add(csma.Install(NodeContainer(switches.Get(1), switches.Get(2))));
-    interSwitch.Add(csma.Install(NodeContainer(switches.Get(0), switches.Get(2))));
-
-    // Instalar la pila de protocolos de Internet en los nodos y switches
+    // Instalar la pila de Internet en todos los nodos
     InternetStackHelper internet;
-    internet.Install(nodos);
-    internet.Install(switches);
+    internet.Install(system1Nodes);
+    internet.Install(system2Nodes);
+    internet.Install(system1Switch);  // Aseguramos que el switch tenga la pila IPv4
+    internet.Install(system2Switch);
+    internet.Install(centralSwitch);
 
-    // Asignar direcciones IP a los dispositivos conectados
+    // Crear enlaces punto a punto entre nodos y switches
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    std::vector<NetDeviceContainer> system1Devices(3), system2Devices(3);
+
+    // Enlace servidor-switch para sistema 1
+    system1Devices[0] = pointToPoint.Install(system1Nodes.Get(0), system1Switch.Get(0));
+    // Enlaces cliente-switch para sistema 1
+    for (uint32_t i = 1; i <= 3; ++i) {
+        system1Devices[i - 1] = pointToPoint.Install(system1Nodes.Get(i), system1Switch.Get(0));
+    }
+
+    // Repetir para el sistema 2
+    system2Devices[0] = pointToPoint.Install(system2Nodes.Get(0), system2Switch.Get(0));
+    for (uint32_t i = 1; i <= 3; ++i) {
+        system2Devices[i - 1] = pointToPoint.Install(system2Nodes.Get(i), system2Switch.Get(0));
+    }
+
+    // Crear enlace CSMA entre los switches y el switch central
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", StringValue("1Gbps"));
+    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(500)));
+
+    NetDeviceContainer switchLinks;
+    switchLinks.Add(csma.Install(NodeContainer(system1Switch.Get(0), centralSwitch.Get(0))));
+    switchLinks.Add(csma.Install(NodeContainer(system2Switch.Get(0), centralSwitch.Get(0))));
+
+    // Asignar direcciones IP
     Ipv4AddressHelper ipv4;
-    ipv4.SetBase("10.1.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer interfacesSwitch1 = ipv4.Assign(dispositivosSwitch1);
+    std::vector<Ipv4InterfaceContainer> system1Interfaces(3), system2Interfaces(3);
 
-    ipv4.SetBase("10.1.2.0", "255.255.255.0");
-    Ipv4InterfaceContainer interfacesSwitch2 = ipv4.Assign(dispositivosSwitch2);
+    for (uint32_t i = 0; i < 3; ++i) {
+        std::ostringstream subnet;
+        subnet << "10.1." << i + 1 << ".0";
+        ipv4.SetBase(subnet.str().c_str(), "255.255.255.0");
+        system1Interfaces[i] = ipv4.Assign(system1Devices[i]);
 
-    ipv4.SetBase("10.1.3.0", "255.255.255.0");
-    Ipv4InterfaceContainer interfacesSwitch3 = ipv4.Assign(dispositivosSwitch3);
+        subnet.str("");  // Limpiar el stream para el sistema 2
+        subnet << "10.2." << i + 1 << ".0";
+        ipv4.SetBase(subnet.str().c_str(), "255.255.255.0");
+        system2Interfaces[i] = ipv4.Assign(system2Devices[i]);
+    }
 
-    ipv4.SetBase("10.1.4.0", "255.255.255.0");
-    ipv4.Assign(interSwitch);  // Direcciones IP para enlaces entre switches
+    // Configurar servidores TCP en los nodos servidores
+    uint16_t port = 50000;
+    PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+    ApplicationContainer serverApps;
 
-    // Llenar las tablas de enrutamiento globalmente
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+    serverApps.Add(sinkHelper.Install(system1Nodes.Get(0)));  // Servidor sistema 1
+    serverApps.Add(sinkHelper.Install(system2Nodes.Get(0)));  // Servidor sistema 2
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
 
-    // Configurar aplicaciones para generar tráfico entre nodos
-    uint16_t puerto = 50000;  // Puerto base para las conexiones
-    NodeContainer clientes(nodos.Get(0), nodos.Get(2), nodos.Get(4));
-    NodeContainer servidores(nodos.Get(1), nodos.Get(3), nodos.Get(5));
-    ConfigurarAplicaciones(clientes, servidores, interfacesSwitch1, puerto);
+    // Configurar clientes TCP en los nodos clientes
+    for (uint32_t i = 1; i <= 3; ++i) {
+        BulkSendHelper clientHelper("ns3::TcpSocketFactory",
+                                    InetSocketAddress(system1Interfaces[0].GetAddress(0), port));
+        ApplicationContainer clientApp = clientHelper.Install(system1Nodes.Get(i));
+        clientApp.Start(Seconds(2.0));
+        clientApp.Stop(Seconds(10.0));
 
-    // Habilitar capturas PCAP para analizar el tráfico de red
-    csma.EnablePcap("nodos", nodos, true);
-    csma.EnablePcap("switches", switches, true);
+        clientHelper.SetAttribute("Remote", 
+                                  AddressValue(InetSocketAddress(system2Interfaces[0].GetAddress(0), port)));
+        clientApp = clientHelper.Install(system2Nodes.Get(i));
+        clientApp.Start(Seconds(2.0));
+        clientApp.Stop(Seconds(10.0));
+    }
 
-    // Establecer el tiempo de parada de la simulación
-    Simulator::Stop(Seconds(10.0));
+    // Habilitar trazas ASCII y PCAP
+    AsciiTraceHelper ascii;
+    pointToPoint.EnableAsciiAll(ascii.CreateFileStream("second.tr"));
+    csma.EnablePcapAll("second");
+
+    // Monitor de flujo
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll();
 
     // Iniciar la simulación
+    Simulator::Stop(Seconds(12.0));
     Simulator::Run();
 
-    // Limpiar recursos una vez terminada la simulación
-    Simulator::Destroy();
+    // Generar estadísticas en XML
+    monitor->CheckForLostPackets();
+    monitor->SerializeToXmlFile("second.xml", true, true);
 
+    Simulator::Destroy();
     return 0;
 }
